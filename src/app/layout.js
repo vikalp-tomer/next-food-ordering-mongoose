c4cc7c25a7dd38dd6c1f48ec7b6b444007f4b280
vikalp-tomer <vikalptomar15@gmail.com>
2024-07-22 21:35:53 +0530

    homepage completed

--- src/app/layout.js
+++ src/app/layout.js
@@ -11,7 +11,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <main className="max-w-4xl mx-auto p-4">{children}</main>
+      </body>
     </html>
   );
 }
---
+++ src/components/menu/MenuItem.js
@@ -0,0 +1,15 @@
+export default function MenuItem() {
+  return (
+    <div className="bg-gray-200 p-4 rounded-lg text-center hover:bg-white hover:shadow-2xl hover:shadow-black/25 transition-all">
+      <div className="text-center">
+        <img className="max-h-auto max-h-24 block mx-auto" src="/pizza.png" alt="pizza" />
+      </div>
+      <h4 className="font-semibold text-xl my-3">Pepperoni Pizza</h4>
+      <p className="text-gray-500 text-sm">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus dolores ipsum accusamus
+        blanditiis aut voluptates!
+      </p>
+      <button className="bg-primary text-white rounded-full px-8 py-2 mt-4">Add to cart $12</button>
+    </div>
+  );
+}
